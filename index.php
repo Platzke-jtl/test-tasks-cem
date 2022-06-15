8cea17a3ce447e369b8087f12549229720f97457
Cem Aktas <cem.aktas@forkon.de>
2022-06-15 16:31:12 +0200

    - // add logic

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require_once "./Caching.php";
+use Caching\Caching;
+
+//$start_time = microtime(true);
+
+$cache = new Caching();
+
+if ($cache->has("departure"))
+{
+    $singleDeparture = $cache->get("departure");
+    echo nl2br($singleDeparture);
+} else {
+    $departures = "departures.txt";
+    $file = fopen($departures, 'r');
+
+    while (!feof($file)) {
+        $singleDeparture = fgets($file);
+        echo nl2br($singleDeparture);
+        $cache->set("departure", $singleDeparture);
+    }
+    fclose($file);
+}
+
+//$end_time = microtime(true);
+//$execution_time = ($end_time - $start_time);
+//echo nl2br("Total execution time" . $execution_time);
